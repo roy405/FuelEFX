7c99482b5db7a29160fbe78f2d309715c8b29347
Kawshik Roy <kawshik.zero@yahoo.com>
2023-09-03 17:36:36 +1000

    - Added Form Views layout for both Trip and Fuel - Added Functions to validation and error handling for Form entries - Added functions to submit entries connecting with ViewModel Functions

--- FuelEFX/View/Forms/FuelForm.swift
+++ FuelEFX/View/Forms/FuelForm.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct FuelForm: View {
+    @ObservedObject var viewModel: FuelStore
+    
+    @State private var date = Date()
+    @State private var odometerReading = ""
+    @State private var fuelAmount = ""
+    @State private var fuelCost = ""
+    @State private var fuelType = ""
+    @State private var location = ""
+    @State private var notes = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Fuel Entry Details")) {
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Odometer Reading", text: $odometerReading)
+                    .keyboardType(.numberPad)
+                TextField("Fuel Amount", text: $fuelAmount)
+                    .keyboardType(.decimalPad)
+                TextField("Fuel Cost", text: $fuelCost)
+                    .keyboardType(.decimalPad)
+                TextField("Fuel Type", text: $fuelType)
+                TextField("Location", text: $location)
+            }
+            
+            Section(header: Text("Notes")) {
+                TextEditor(text: $notes)
+                    .frame(height: 100)
+            }
+            
+            Button("Save") {
+                saveFuelEntry()
+            }
+        }
+        .navigationTitle("Add Fuel Entry")
+    }
+    
+    
+    
+    private func saveFuelEntry() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM yy"
+        guard let odometer = Double(odometerReading),
+              let amount = Double(fuelAmount),
+              let cost = Double(fuelCost) else {
+            // Handle invalid input
+            fatalError("Invalid Input! Please fill form fields with appropriate data.")
+        }
+        
+        let stringDate = dateFormatter.string(from: date)
+        
+        let fuelRecord = Fuel(id: 0, refillDate: stringDate, odometerReading: odometer, fuelAmount: amount, fuelCost: cost, fuelType: fuelType, refillLocation: location, notes: notes)
+        
+        viewModel.addRecord(fuelRecord)
     }
 }
 
 struct FuelForm_Previews: PreviewProvider {
     static var previews: some View {
-        FuelForm()
+        FuelForm(viewModel: FuelStore())
     }
 }
--- FuelEFX/View/Forms/TripForm.swift
+++ FuelEFX/View/Forms/TripForm.swift
@@ -8,13 +8,65 @@
 import SwiftUI
 
 struct TripForm: View {
+    @ObservedObject var viewModel: TripStore
+    
+    @State private var date = Date()
+    @State private var startOdometer = ""
+    @State private var endOdometer = ""
+    @State private var startLocation = ""
+    @State private var endLocation = ""
+    @State private var purpose = ""
+    @State private var notes = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Fuel Entry Details")) {
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Start Odometer Reading", text: $startOdometer)
+                    .keyboardType(.decimalPad)
+                TextField("End Odometer Reading", text: $endOdometer)
+                    .keyboardType(.decimalPad)
+                TextField("Start Location", text: $startLocation)
+                TextField("End Location", text: $endLocation)
+                TextField("Purpose", text: $purpose)
+            }
+            
+            Section(header: Text("Notes")) {
+                TextEditor(text: $notes)
+                    .frame(height: 100)
+            }
+            
+            Button("Save") {
+                saveTripEntry()
+            }
+        }
+        .navigationTitle("Add Fuel Entry")
+      }
+    
+    private func saveTripEntry(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM yy"
+        guard let odometerStart = Double(startOdometer),
+              let odometerEnd = Double(endOdometer) else {
+            fatalError("Invalid Input! Please fill form fields with appropriate data.")
+        }
+        let stringDate = dateFormatter.string(from: date)
+        
+        let tripRecord = Trip(id: 0,
+                             tripDate: stringDate,
+                             startOdometer: odometerStart,
+                             endOdometer: odometerEnd,
+                             startLocation: startLocation,
+                             endLocation: endLocation,
+                             purpose: purpose,
+                             notes: notes)
+        viewModel.addRecord(tripRecord)
     }
 }
 
+
 struct TripForm_Previews: PreviewProvider {
     static var previews: some View {
-        TripForm()
+        TripForm(viewModel: TripStore())
     }
 }
