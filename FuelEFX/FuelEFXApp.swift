8a0886a67b6edf65a91a9383accb73d4cecf4740
Kawshik Roy <kawshik.zero@yahoo.com>
2023-09-03 16:45:50 +1000

    Initial Commit

---
+++ FuelEFX/FuelEFXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEFXApp.swift
+//  FuelEFX
+//
+//  Created by Cube on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FuelEFXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
