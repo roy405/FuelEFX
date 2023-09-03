76378b2d845667733a93f4d4550e6a76d9c0d164
Kawshik Roy <kawshik.zero@yahoo.com>
2023-09-03 17:40:25 +1000

    - Added the Home Screen view for charts (TBD) - Added Fuel and Trip Json Files

--- FuelEFX/View/Home.swift
+++ FuelEFX/View/Home.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack(spacing: 20) {
+            Text("SwiftUI Charts")
+                .font(.largeTitle)
+                .padding(.top, 20)
+                        
+                // First Chart View
+                VStack {
+                    Text("Line Chart")
+                        .font(.title)
+
+                }
+                .background(Color.white)
+                .cornerRadius(12)
+                .shadow(radius: 4)
+                        
+                // Second Chart View
+                VStack {
+                    Text("Pie Chart")
+                        .font(.title)
+                }
+                .background(Color.white)
+                .cornerRadius(12)
+                .shadow(radius: 4)
+                        
+                Spacer()
+            }
+        .padding()
+        }
 }
 
 struct Home_Previews: PreviewProvider {
