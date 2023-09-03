619163c318b7370d2ed31d8505b3b3e306858406
Kawshik Roy <kawshik.zero@yahoo.com>
2023-09-03 17:27:42 +1000

    - Added Fuel and Trip Detail Views

--- FuelEFX/View/DetailView/FuelDetailView.swift
+++ FuelEFX/View/DetailView/FuelDetailView.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct FuelDetailView: View {
+    @EnvironmentObject var fuelStore: FuelStore
+    @State private var isEditing = false
+    @State private var editedFuelRecord: Fuel
+    
+    var fuel: Fuel
+    
+    init(fuel: Fuel) {
+        self.fuel = fuel
+        self._editedFuelRecord = State(initialValue: fuel)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Fuel Record Details")) {
+                Text("Date: \(formattedDate)")
+                Text("Odometer Reading: \(editedFuelRecord.odometerReading)")
+                Text("Fuel Amount: \(editedFuelRecord.fuelAmount) liters")
+                Text("Fuel Cost: $\(editedFuelRecord.fuelCost)")
+                Text("Fuel Type: \(editedFuelRecord.fuelType)")
+                Text("Location: \(editedFuelRecord.refillLocation)")
+            }
+            
+            Section(header: Text("Notes")) {
+                Text(editedFuelRecord.notes)
+            }
+            
+            if isEditing {
+                Button("Save Changes") {
+                    saveChanges()
+                }
+            } else {
+                Button("Edit") {
+                    isEditing = true
+                }
+            }
+            
+            Button("Delete") {
+                deleteFuelRecord()
+            }
+        }
+        .navigationBarTitle("Fuel Record Details")
+    }
+    
+    private func saveChanges() {
+        fuelStore.editRecord(editedFuelRecord)
+        
+        isEditing = false
+    }
+    
+    private func deleteFuelRecord() {
+        fuelStore.deleteRecord(editedFuelRecord)
+    }
+    
+    private var formattedDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: fuel.refillDate)
     }
 }
 
 struct FuelDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FuelDetailView()
+        let fuelStore = FuelStore()
+        FuelDetailView(fuel: fuelStore.records[0])
     }
 }
--- FuelEFX/View/DetailView/TripDetailView.swift
+++ FuelEFX/View/DetailView/TripDetailView.swift
@@ -8,13 +8,69 @@
 import SwiftUI
 
 struct TripDetailView: View {
+    @EnvironmentObject var tripStore: TripStore
+    @State private var isEditing = false
+    @State private var editedTripRecord: Trip
+    
+    var trip: Trip
+    
+    init(trip: Trip) {
+        self.trip = trip
+        self._editedTripRecord = State(initialValue: trip)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Trip Record Details")) {
+                Text("Date: \(formattedDate)")
+                Text("Start Odometer Reading: \(editedTripRecord.startOdometer)")
+                Text("End Odometer Reading: \(editedTripRecord.endOdometer)")
+                Text("Start Location: \(editedTripRecord.startLocation)")
+                Text("End Location: \(editedTripRecord.endLocation)")
+                Text("Purpose: \(editedTripRecord.purpose)")
+            }
+            
+            Section(header: Text("Notes")) {
+                Text(editedTripRecord.notes)
+            }
+            
+            if isEditing {
+                Button("Save Changes") {
+                    saveChanges()
+                }
+            } else {
+                Button("Edit") {
+                    isEditing = true
+                }
+            }
+            
+            Button("Delete") {
+                deleteTripRecord()
+            }
+        }
+        .navigationBarTitle("Trip Record Details")
+    }
+    
+    private func saveChanges() {
+        tripStore.editRecord(editedTripRecord)
+        
+        isEditing = false
+    }
+    
+    private func deleteTripRecord() {
+        tripStore.deleteRecord(editedTripRecord)
+    }
+    
+    private var formattedDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: trip.tripDate)
     }
 }
 
 struct TripDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TripDetailView()
+        let tripStore = TripStore()
+        TripDetailView(trip: tripStore.records[0])
     }
 }
